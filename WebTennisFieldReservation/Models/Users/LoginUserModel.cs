abf91bd8079ef236673f9b2bf4621afef5274107
Michele <Michele@MICHELE-PC>
2023-04-21 16:18:28 +0200

    starting login

---
+++ WebTennisFieldReservation/Models/Users/LoginUserModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebTennisFieldReservation.Models.Users
+{
+    public class LoginUserModel
+    {       
+        [Required(ErrorMessage = "{0} is required")]
+        [StringLength(64, ErrorMessage = "{0} must be up to {1} chars long")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Not a valid email address")]
+        [DisplayName("Email address")]       
+        public string Email { get; set; } = null!;       
+
+        [Required(ErrorMessage = "{0} is required")]
+        [StringLength(64, ErrorMessage = "{0} must be up to {1} chars long")]
+        [DataType(DataType.Password)]
+        [DisplayName("Password")]
+        public string Password { get; set; } = null!;
+    }
+}
