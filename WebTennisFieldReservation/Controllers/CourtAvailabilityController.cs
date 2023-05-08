1779ad856162358cdf19a1b12a577e6d8cdfbf45
Michele <Michele@MICHELE-PC>
2023-05-08 17:22:34 +0200

    implementing reservation slot selection. Added checkbox styling.

---
+++ WebTennisFieldReservation/Controllers/CourtAvailabilityController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebTennisFieldReservation.Data;
+
+namespace WebTennisFieldReservation.Controllers
+{
+	[Route("courtavailability")]
+	public class CourtAvailabilityController : Controller
+	{
+		private readonly ICourtComplexRepository _repo;
+
+		public CourtAvailabilityController(ICourtComplexRepository repo)
+		{
+			_repo = repo;
+		}
+		
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
